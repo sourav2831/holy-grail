256103eaa32bf54414dcaf6311d36a95c9025c9a
Sourav Tiwari <souravtiwari@Souravs-MacBook-Pro.local>
2023-07-02 02:21:50 +0530

    implemented holy grail

---
+++ src/HolyGrail.js
@@ -0,0 +1,15 @@
+import "./index.css";
+
+const HolyGrail = () => {
+  return (
+    <div className="container">
+      <header>Header</header>
+      <div className="left-sidebar">This is Left Sidebar </div>
+      <div className="main">Main</div>
+      <div className="right-sidebar">This is Right Sidebar</div>
+      <footer>Footer</footer>
+    </div>
+  );
+};
+
+export default HolyGrail;
